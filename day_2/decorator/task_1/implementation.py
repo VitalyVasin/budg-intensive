bb5b6beaf8c3a14f93aaf918da0be59c2eb09bc3
vit <tomas_anderson@mail.ru>
2021-08-11 02:26:46 +0300

    Homework Day 2, decorator, task 1

--- day_2/decorator/task_1/implementation.py
+++ day_2/decorator/task_1/implementation.py
@@ -1,6 +1,35 @@
-def time_execution():
+from day_2.common import factorial
+import time
+
+
+def time_execution(func):
     """
     Обертка, печатающая время выполнения функции.
     """
+    def subfunc(*args, **kwargs):
+        start_time = time.time()
+        result_func = func(*args, **kwargs)
+        end_time = time.time()
+        result_time = round(end_time - start_time, 4)
+        print('Функция работала ', result_time, ' секунд')
+        return result_func
+    return subfunc
 
-    raise NotImplementedError
+
+@time_execution
+def factorial(number):
+    """
+    Возвращает факториал переданного числа
+    Args:
+        number: число, для которого надо посчитать факториал
+
+    Returns:
+        product - int - факториал от number
+    """
+    product = 1
+    for element in range(1, number+1):
+        product *= element
+
+    return product
+
+test = factorial(11111)
