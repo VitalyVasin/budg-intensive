62540cf81a0de910e897e3bc031b5d5066a27ec6
vit <tomas_anderson@mail.ru>
2021-08-10 23:59:33 +0300

    Homework Day 2, buildin_function, task 1

--- day_2/builtin_functions/task_1/implementation.py
+++ day_2/builtin_functions/task_1/implementation.py
@@ -1,2 +1,26 @@
+from day_2.common import (
+    MyException,
+)
+
 class Value:
-    pass
+    def __init__(self, argument):
+        self.argument = argument
+
+    def __add__(self, other):
+        return other + self.argument
+
+    def __sub__(self, other):
+        return self.argument - other
+
+    def __mul__(self, other):
+        return self.argument * other
+
+    def __truediv__(self, other):
+        try:
+            div = self.argument / other
+
+        except ZeroDivisionError:
+            raise MyException
+        return div
+
+
