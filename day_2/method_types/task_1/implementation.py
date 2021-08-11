2970328fcf4e7fea953f2efc96b8b61ee56e7722
vit <tomas_anderson@mail.ru>
2021-08-11 05:20:20 +0300

    Homework Day 2, method_types, task 1-2 (not work)

--- day_2/method_types/task_1/implementation.py
+++ day_2/method_types/task_1/implementation.py
@@ -1,2 +1,25 @@
 class Coffee:
-    pass
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    def __str__(self):
+        return 'Coffee Class'
+
+    @classmethod
+    def get_cappuchino(cls):
+        ingredients = ['coffee', 'milk', 'sugar']
+        return cls(ingredients)
+
+    @classmethod
+    def get_latte(cls):
+        ingredients = ['coffee', 'milk']
+        return cls(ingredients)
+
+    @classmethod
+    def get_ice_coffee(cls):
+        ingredients = ['coffee', 'milk', 'ice cream']
+        return cls(ingredients)
+
+if __name__ == '__main__':
+    c = Coffee.get_latte()
+    print(c)
--- day_2/method_types/task_2/implementation.py
+++ day_2/method_types/task_2/implementation.py
@@ -1,11 +1,20 @@
 class ClassFather:
-    pass
+    try:
+        def get_name(self, name):
+            self.name = name
+
+        def register(self):
+            pass
+    except MyException:
+        raise MyException
+
+
 
 
 class User1(ClassFather):
-    _name = ''
+    _name = 'Chip'
 
 
 class User2(ClassFather):
-    _name = ''
+    _name = 'Dale'
 
