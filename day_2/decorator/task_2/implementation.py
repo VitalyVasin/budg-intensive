cc28c7636d4287ab531daf75c57693156eb55856
vit <tomas_anderson@mail.ru>
2021-08-11 03:23:03 +0300

    Homework Day 2, decorator, task 2

--- day_2/decorator/task_2/implementation.py
+++ day_2/decorator/task_2/implementation.py
@@ -1,7 +1,21 @@
-def check_value():
+from day_2.common import MyException
+
+def check_value(func):
     """
     Обертка, проверяющая валидность переданного значения(неотрицательный int).
     В случае валидного значения - передает дальше в функцию,
     в противном случае - выбрасывает исключение MyException.
     """
-    raise NotImplementedError
+    def sub_func(a):
+        try:
+            if (type(a) != int) or (a == None) or a < 0:
+                raise MyException
+            else:
+                func(a)
+        except MyException:
+            raise MyException
+
+        return func(a)
+
+    return sub_func
+
