c93593653c5f1c134ce00769bcd70c1e811e5afa
vit <tomas_anderson@mail.ru>
2021-08-15 17:59:36 +0300

    Homework Day 5, generators, task 1

--- day_5/generators/task1/implementation.py
+++ day_5/generators/task1/implementation.py
@@ -3,4 +3,14 @@ def fib(n):
     """
     Функция, которая возвращает n-ое число последовательности Фибоначчи
     """
-    raise NotImplementedError
+    num0, num1 = 1, 1
+
+    for num in range(n):
+        yield num0
+        num_fib = num0 + num1
+        num0 = num1
+        num1 = num_fib
+
+list_fib = list(fib(10))
+result_fib = list_fib[-1]
+print(result_fib)
